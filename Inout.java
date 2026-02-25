96a213a89b71bae26a0c559cdba02e3a5d528150
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-02-25 19:14:53 +0530

    done

---
+++ Inout.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+
+public class Inout { 
+        public static void main(String[] args) {
+            Scanner std = new Scanner(System.in);
+            int a = std.nextInt();
+            int b = std.nextInt();
+            int sum = a + b ;
+            System.out.println(sum);
+        }
+}
