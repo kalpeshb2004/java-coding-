e66b3e85a1ac3cc8cdf3f6a2976e603ca98fe940
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-02-25 19:47:44 +0530

    done

--- Inout.java
+++ Inout.java
@@ -4,10 +4,11 @@ import java.util.*;
 
 public class Inout { 
         public static void main(String[] args) {
-            Scanner std = new Scanner(System.in);
-            int a = std.nextInt();
-            int b = std.nextInt();
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
             int sum = a + b ;
             System.out.println(sum);
         }
+        
 }
