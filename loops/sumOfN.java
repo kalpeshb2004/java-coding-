298fda608275978b5c4ae3a218e9a7a5ebd73b95
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-02-26 21:05:29 +0530

    sum of first N numbers

--- loops/sumOfN.java
+++ loops/sumOfN.java
@@ -1,3 +1,15 @@
+
+import java.util.Scanner;
+
 public class sumOfN {
-    
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int num = x.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
 }
