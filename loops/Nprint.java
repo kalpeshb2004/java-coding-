3161d2ad7a53ec404d165819beaae3dd9e23de17
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-02-26 20:58:29 +0530

    first 1 to N number

---
+++ loops/Nprint.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+class Nprint {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int num = x.nextInt();
+         
+        int sum = 0;
+        for (int i=1; i<=num; i++) {
+              System.out.println(i);
+        }
+
+    }
+}
