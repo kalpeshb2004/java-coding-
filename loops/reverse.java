b6851ca844340ba5948e74e77df1f3d89a97f43f
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-02-27 17:59:16 +0530

    logic learned

---
+++ loops/reverse.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class reverse {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int num = x.nextInt();
+        int a = x.nextInt();
+        int b = x.nextInt();
+
+        for (int i = 0; i <= num; i++) {
+            
+        }
+        System.out.println(b);
+    }
+}
