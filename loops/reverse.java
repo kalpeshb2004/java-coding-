3a9506fcd53b6dd4f27c2fb7ea204eba68244010
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-02-27 17:59:40 +0530

    commited

--- loops/reverse.java
+++ loops/reverse.java
@@ -5,12 +5,15 @@ public class reverse {
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
         int num = x.nextInt();
-        int a = x.nextInt();
-        int b = x.nextInt();
+        
+        int rev = 0;
+        
+        while (num != 0) {
+             int temp = num % 10; // will gives last digit
+             rev = rev * 10 + temp; // shift values from left to right and add given above values in  left sde
+             num = num / 10; // last digits still on right side then it will remove it .
 
-        for (int i = 0; i <= num; i++) {
-            
         }
-        System.out.println(b);
+        System.out.println(rev);
     }
 }
