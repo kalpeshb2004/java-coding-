8685d96d214ec9bd7a4135b5e0e702b2da2b7a9b
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-02-27 20:53:37 +0530

    half traingle formed

---
+++ patternStar/halfTraingle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class halfTraingle {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int n = x.nextInt();
+
+        // outer loop
+        for (int i=1; i<=n; i++) {
+            //inner loop
+            for (int j = 1; j <= i; j++) {
+                //cell
+                System.out.print("*");  // orinting * star
+            }
+            System.out.println(); // move to the next line
+        }
+    }
+}
