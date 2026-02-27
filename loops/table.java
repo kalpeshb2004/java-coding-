f3e98111a23f4d7cba21cc88374632be4f217bb1
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-02-27 18:31:46 +0530

    multiplication table creation

---
+++ loops/table.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class table {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in); 
+        int num = x.nextInt();
+
+        for (int i = 1; i<=10; i++) {
+              System.out.println(num * i);
+        }
+
+    }
+    
+
+    
+}
