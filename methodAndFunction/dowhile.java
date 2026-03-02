4d33627bbef507c668c6e11002a9deb2a99277fa
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-02 18:18:49 +0530

    do whjile loop with infinite

---
+++ methodAndFunction/dowhile.java
@@ -0,0 +1,11 @@
+public class dowhile {
+    public static void infinite() {
+        do { 
+            System.out.println("infinite loop");
+        } while (true);
+    }
+
+    public static void main(String[] args) {
+        infinite();
+    }
+}
