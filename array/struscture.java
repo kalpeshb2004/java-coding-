d05382c71d2443530c93b587e52ea7aa4afcdecd
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-03 19:53:26 +0530

    basic structure of array input/output

---
+++ array/struscture.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class struscture {
+  public static void main(String[] args) {
+      Scanner x = new Scanner(System.in);
+      int size = x.nextInt();
+
+      int number[] = new int[size];  // array ka object banaya aur size lega "size" varaible me via input
+      
+      //array input
+    for (int i = 0; i < size; i++) {
+        number[i] = x.nextInt();
+    }
+
+    //array output
+    for (int i = 0; i < size; i++) {
+        System.out.println(number[i
+        ]);
+    }
+
+
+  }  
+}
