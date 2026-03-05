7e123712509cd85fd475b4c0e5c8882bbfa2b2c7
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-05 18:58:26 +0530

    string function

--- strings.java/structureString.java
+++ strings.java/structureString.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class structureString {
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
-        //input
+       
+         //input
         String fname = x.nextLine();
         String lname = x.nextLine();
 
@@ -19,6 +20,21 @@ public class structureString {
         for (int i = 0; i < Cname.length(); i++) {
             System.out.print(" " +Cname.charAt(i));
         }
+        System.out.println();
+
+        //compare to 
+        if(fname.compareTo(lname) == 0){
+            System.out.println("both String are equal");
+        }else{
+            System.out.println("Strings are not equal");
+        }
+
+
+        //sub string
+        String name = "hello welcome to AI";
+        String nname = name.substring(6 , name.length());
+        System.out.println(nname);
+
 
        
         
