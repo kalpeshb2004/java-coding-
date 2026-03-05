97356f7dc3d825aa042f58536b9f9302b1d4ed1e
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-05 19:15:21 +0530

    string with array calculation

---
+++ strings.java/stringArrayLength.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class stringArrayLength {
+    public static void main(String[] args) {
+        Scanner x= new Scanner(System.in);
+        int size = x.nextInt();
+        x.nextLine(); // bec conversion from stint to int gets difficult
+
+        // array creation
+        String name[] = new String[size];
+
+        int sum = 0;
+        for (int i = 0; i <size; i++) {
+            name[i] = x.nextLine(); // array input
+            sum = sum + name[i].length(); // array length calculation
+        }
+        System.out.println(sum);
+
+    }
+}
