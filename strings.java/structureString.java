82b9d2038904e66057305066bcad136f3910cac3
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-05 17:59:29 +0530

    fname print

---
+++ strings.java/structureString.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class structureString {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        String fname = x.nextLine();
+
+        System.out.println("fname :" + fname );
+
+        //String lname = x.nextLine();
+        
+
+    }
+}
