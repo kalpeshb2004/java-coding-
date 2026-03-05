455ad272f33de0aad3160669ed66fb66547c94ec
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-05 18:05:07 +0530

    fname print concatenation charAt

--- strings.java/structureString.java
+++ strings.java/structureString.java
@@ -4,11 +4,23 @@ import java.util.Scanner;
 public class structureString {
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
+        //input
         String fname = x.nextLine();
+        String lname = x.nextLine();
 
-        System.out.println("fname :" + fname );
+        //concatenation.
+        String Cname = fname +" "+ lname ;
+        System.out.println("Cname :" + Cname );
 
-        //String lname = x.nextLine();
+        //length
+        System.out.println("length :" + Cname.length());
+
+        //charAt each char print separate
+        for (int i = 0; i < Cname.length(); i++) {
+            System.out.print(" " +Cname.charAt(i));
+        }
+
+       
         
 
     }
