94970c4276beaafb853838a769a273ba7c95781e
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-06 18:21:20 +0530

    hasNext method use to ieterate until the end fo file

--- hackerrank/Q9.java
+++ hackerrank/Q9.java
@@ -1,19 +1,27 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 
 public class Q9 {
 
     public static void main(String[] args) {
        Scanner x= new Scanner(System.in);
-       int n =1;
+       int n = 1;
        
-       while(x.hasNext()) {
-        System.out.println(n++ + " " + x.nextLine());
-       }
-       x.close();
+       while(x.hasNext()) //hasNext() method use to iterate a loop until end of input
+            {
+                System.out.println(n++ + " " + x.nextLine());
+            }
+            x.close();
     }
 }
+/*Sample Input
+
+Hello world
+I am a file
+Read me until end-of-file.
+
+Sample Output
+
+1 Hello world
+2 I am a file
+3 Read me until end-of-file. */
