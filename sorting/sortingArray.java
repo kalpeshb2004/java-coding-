7fb94e37b2e2e5e67e1a05917f6a10e1d932c5c7
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-10 20:17:11 +0530

    sorting values in array

---
+++ sorting/sortingArray.java
@@ -0,0 +1,24 @@
+public class sortingArray {
+    public static void print(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] +" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {6,4,8,3,5,1};
+
+        for (int i = 0; i <arr.length-1; i++) { // iteraration = n-1.
+            for (int j = 0; j < arr.length-i-1; j++) { // iteraration = n-1. but last bigger value wont be repeat thats why -i taken. 
+                //swapping of numbers greater number last and lower number first
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        print(arr);
+
+    }
+}
