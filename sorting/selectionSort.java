dfe2b998b8c4fc76ea688e058cc824a37ed7b84c
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-10 21:09:16 +0530

    array values sorting using selection metghod

---
+++ sorting/selectionSort.java
@@ -0,0 +1,24 @@
+public class selectionSort {
+    public static void print(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] +" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {3,6,2,7,1};
+
+        for (int i = 0; i < arr.length-1; i++) {
+            int smallest = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        print(arr);
+    }
+}
--- sorting/sortingArray.java
+++ sorting/sortingArray.java
@@ -8,6 +8,7 @@ public class sortingArray {
     public static void main(String[] args) {
         int arr[] = {6,4,8,3,5,1};
 
+        //bubble sort
         for (int i = 0; i <arr.length-1; i++) { // iteraration = n-1.
             for (int j = 0; j < arr.length-i-1; j++) { // iteraration = n-1. but last bigger value wont be repeat thats why -i taken. 
                 //swapping of numbers greater number last and lower number first
