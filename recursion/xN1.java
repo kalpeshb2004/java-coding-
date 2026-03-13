b8071d10bb0cc137a778caf2c58406f18399942f
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-13 14:23:24 +0530

    x^n stack height : logn

--- recursion/xN1.java
+++ recursion/xN1.java
@@ -1,4 +1,4 @@
-public class xN {
+public class xN1 {
     public static int print(int x,int n){
         if(x==0){
             return 1;
@@ -6,13 +6,17 @@ public class xN {
         if(n==0){
             return 1;
         }
-        // formula is only for getting stack height = n
-        int result1 = print(x, n-1);
-        int result2 = x * result1;
-        return result2;
+        //all program is same just stack hight = logn
+        //if n is even
+        if(n % 2 == 0){
+            return print(x, n/2) * print(x, n/2);
+        }else{
+            return print(x, n/2) * print(x, n/2) * x;
+        }
     }
     public static void main(String[] args) {
        int ans =  print(2, 5);
         System.out.println(ans);
     }
 }
+
