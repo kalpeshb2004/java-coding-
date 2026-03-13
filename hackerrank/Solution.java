1daba1b3d9badfdb765b9040c7de03b9173bbc8b
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-13 19:22:31 +0530

    calendar , get , set getInstance() all of this new concept working with dates and times

---
+++ hackerrank/Solution.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Q13 {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+          
+        // array of weeks 
+         String day_of_week[] = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+         
+         //calendar : built in class that works with date and times
+         Calendar cal = Calendar.getInstance();
+         
+         //.set : setting a specific days on basis of year, month-1, day
+         cal.set(year, month-1, day);
+         
+         //cal.get getting days from array
+         int p = cal.get(Calendar.DAY_OF_WEEK);
+         
+         //return day.  array start from 0 so p-1 manages that confliction
+         return day_of_week[p-1];
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
