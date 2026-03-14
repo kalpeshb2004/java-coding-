d353e824a164eec93610e5dcf9cebaf785a4df8f
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-14 21:01:10 +0530

    specific char set to the end of string

---
+++ recursion/charAtEnd.java
@@ -0,0 +1,27 @@
+public class charAtEnd {
+    public static void print(int i , String str , int count, char element,String newStr){
+        if(i == str.length()){
+            for ( i = 0; i < count; i++) {
+                newStr += element;
+            }
+            System.out.println(newStr);
+            return;
+        }
+        
+        char currentChar = str.charAt(i);
+        if(currentChar == element){
+            count++;
+            print(i+1, str, count, element, newStr);
+        }else{
+            newStr += currentChar;
+            print(i+1, str, count, element, newStr);
+        }
+        
+        
+    }
+    public static void main(String[] args) {
+        String str = "abcxdfrxxvx";
+        print(0, str, 0, 'x', " ");
+        
+    }
+}
