46f1285cb071ad246d4349e37ed846462e350f1c
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-21 22:52:33 +0530

    logic building practice

---
+++ Logic Building/whileLoop/logic1To10.java
@@ -0,0 +1,11 @@
+
+public class logic1To10 {
+    public static void main(String[] args) {
+       int i = 0;
+        while (i < 10){
+            i++;
+            System.out.println(i);
+        }
+        
+    }
+}
