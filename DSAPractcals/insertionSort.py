9fdfac361f815d037e1f379e1be32558a9e261bc
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-26 21:37:27 +0530

    insertion sort

---
+++ DSAPractcals/insertionSort.py
@@ -0,0 +1,13 @@
+arr = [3,1,5,2,5]
+
+n = len(arr)
+
+for i in range(1,n):
+    key = arr[i]
+    j = i - 1
+
+    while j >= 0 and arr[j] > key:
+        arr[j+1] = arr[j]
+        j -= 1
+    arr[j+1] = key
+print("sorted arr : " , arr) 
