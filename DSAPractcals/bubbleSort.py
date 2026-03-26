6a4dd05717054a421ceb9e8d9b111b0fc1cf68b7
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-03-26 21:38:16 +0530

    bubble Sort

---
+++ DSAPractcals/bubbleSort.py
@@ -0,0 +1,10 @@
+arr = [2,5,7,1,4]
+
+n = len(arr)
+
+for i in range(n):
+    for j in range(0,n-i-1):
+       if arr[j] > arr[j+1]:
+           arr[j] , arr[j+1] = arr[j+1] , arr[j]
+print("sorted arr :", arr)           
+           
